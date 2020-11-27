02fef442609836329401f8c251d583d29f3e7eaf
JosiasLopes <jobercardinho2014@gmail.com>
2020-11-27 12:15:10 -0300

    Gerando a 1° tabela com o banco de dados h2(banco de testes)

--- src/main/java/com/cursonc/demo/domain/Categoria.java
+++ src/main/java/com/cursonc/demo/domain/Categoria.java
@@ -2,13 +2,23 @@ package com.cursonc.demo.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	/**
 	 * a classe pode ser convertidas em bytes com serializable
 	 */
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	@Column(name="nome")
 	private String nome;
 	
 	public Categoria() {}
